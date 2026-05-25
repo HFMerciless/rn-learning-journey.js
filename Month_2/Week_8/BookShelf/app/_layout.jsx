58b4d5ff09212d801851871399530d5fb55e2fa9
HF_Merciless <Muhammedgnn46@gmail.com>
2026-05-25 22:29:59 +0300

    Day 8: Solving bugs

--- Month_2/Week_8/BookShelf/app/_layout.jsx
+++ Month_2/Week_8/BookShelf/app/_layout.jsx
@@ -4,6 +4,9 @@ import { useColorScheme } from "react-native"
 import { StatusBar } from "expo-status-bar"
 import { UserProvider } from "../contexts/UserContext"
 
+// TODO: Appwrite project is fully setup and active on cloud panel.
+// Current task: Debug why local context login function state updates do not trigger Expo Router navigation.
+
 export default function RootLayout() {
     const colorScheme = useColorScheme()
     const theme = Colors[colorScheme] ?? Colors.light
