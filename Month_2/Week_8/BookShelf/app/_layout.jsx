74d24641cca39b3478fa90419a0d1717934b4c42
HF_Merciless <Muhammedgnn46@gmail.com>
2026-05-29 23:03:08 +0300

    Day 12: ToDo Update

--- Month_2/Week_8/BookShelf/app/_layout.jsx
+++ Month_2/Week_8/BookShelf/app/_layout.jsx
@@ -4,7 +4,7 @@ import { useColorScheme } from "react-native"
 import { StatusBar } from "expo-status-bar"
 import { UserProvider } from "../contexts/UserContext"
 
-// TODO: Appwrite project is fully setup and active on cloud panel.
+// TODO: Fix the profile layout problem.
 // Current task: Debug why local context login function state updates do not trigger Expo Router navigation.
 
 export default function RootLayout() {
