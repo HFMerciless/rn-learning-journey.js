7700d8453b99f8ea286b70f45790eae50b43d40c
HF_Merciless <Muhammedgnn46@gmail.com>
2026-06-20 10:30:22 +0300

    Day 12: Using axios to fetch

--- Month_3/Week_12/Tanstack Query/Tanstack Query/src/App.tsx
+++ Month_3/Week_12/Tanstack Query/Tanstack Query/src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css'
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+import  axios from "axios";
+
+const [data, setData] = useState([])
 
 function App() {
 useEffect(() => {
-
+  axios.get('https://localhost:8080/todos')
+      .then(response => {setData(response.data)})
+      .catch(error => console.log(error))
 })
   return (
-    <>app</>
+    <>{JSON.stringify(data)}</>
   )
 }
 
