0e49735313158f4462728fd638e3e0af4bbc9f0a
HF_Merciless <Muhammedgnn46@gmail.com>
2026-06-23 11:48:07 +0300

    Day 15: Month 4 setup, fetching requests

---
+++ Month_4/Week_13/Tanstack Query/Tanstack Query/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+// ToDo: SIT AND WORK LIKE A DOG
+
+function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/todos')
+        .then(response => {
+          setData(response.data)
+        })
+        .catch(error => console.log(error))
+  }, [])
+
+  return (
+      <>{JSON.stringify(data)}</>
+  )
+}
+
+export default App
