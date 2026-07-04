697fdc8b8ef9cb22429872ee18b65446464b3655
HF_Merciless <Muhammedgnn46@gmail.com>
2026-07-04 21:33:04 +0300

    Day 26: Debugging

--- Month_4/Week_14/Tanstack Query/src/main.tsx
+++ Month_4/Week_14/Tanstack Query/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.tsx'
 import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-
+//ToDo:Debugging
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <QueryClientProvider>
