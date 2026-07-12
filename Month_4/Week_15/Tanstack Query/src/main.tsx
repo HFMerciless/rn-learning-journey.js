3db3f346dbbc5a1e671a2c81186f1007960742fc
HF_Merciless <Muhammedgnn46@gmail.com>
2026-07-12 23:51:52 +0300

    Day 34: Debugging

--- Month_4/Week_15/Tanstack Query/src/main.tsx
+++ Month_4/Week_15/Tanstack Query/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import "./index.css"
-import App from './App.tsx'
+import App from './App'
 import {QueryClientProvider, QueryClient} from "@tanstack/react-query";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
@@ -13,7 +12,6 @@ const queryClient = new QueryClient({
         },
     },
 })
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
       <QueryClientProvider client={queryClient}>
