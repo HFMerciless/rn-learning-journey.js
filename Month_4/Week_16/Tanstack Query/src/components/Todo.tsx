3ec7483d82768554d4c15f53339e218129cba6a3
HF_Merciless <Muhammedgnn46@gmail.com>
2026-07-17 22:31:35 +0300

    Day 39: Using mutations

--- Month_4/Week_16/Tanstack Query/src/components/Todo.tsx
+++ Month_4/Week_16/Tanstack Query/src/components/Todo.tsx
@@ -1,24 +1,42 @@
 import {useTodos, useTodosIds} from "../services/queries.ts";
+import {useCreateTodo} from "../services/mutations.ts";
+import {type SubmitHandler, useForm} from "react-hook-form";
+import type {Todo} from "../types/todo.ts";
 
-export default function Todo() {
+export default function Todos() {
     const todosIdsQuery = useTodosIds();
     const todosQueries = useTodos(todosIdsQuery.data);
+    const createTodoMutation = useCreateTodo()
+    const handleCreateTodoSubmit: SubmitHandler<Todo> = (data) => {
+        createTodoMutation.mutate(data)
+    }
+    const {register, handleSubmit} =useForm<Todo>()
 
-    return <>
-        {todosIdsQuery.data?.map((id) => (
-            <p key={id}>Id: {id}</p>
-        ))}
-
-        <ul>
-            {todosQueries.map(({data}) => (
-                    <li key={data.id}>
-                        <div>Id: {data?.id}</div>
-                        <span>
-                            <strong>Title: </strong> {data?.title},{" "}
-                            <strong>Description: </strong> {data?.description}
-                        </span>
-                    </li>
-                ))}
-        </ul>
-    </>
+    return (
+        <>
+            <form onSubmit={handleSubmit(handleCreateTodoSubmit)}>
+                <h4>New todo</h4>
+                <input placeholder="title" {...register("title")}/>
+                <br/>
+                <input placeholder="description" {...register("description")}/>
+            </form>
+            <ul>
+                {todosQueries.map(({ data, isLoading }, index) => {
+                    if (isLoading || !data) {
+                        return <li key={`loading-${index}`}>Loading...</li>;
+                    }
+                    return (
+                        <li key={data.id}>
+                            <br />
+                            <div>Id: {data.id}</div>
+                            <span>
+                                <strong>Title: </strong> {data.title},
+                                <strong style={{marginLeft:10}}>Description: </strong> {data.description || "No description"}
+                            </span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </>
+    );
 }
---
+++ Month_4/Week_16/Tanstack Query/src/services/mutations.ts
@@ -0,0 +1,22 @@
+import {useMutation} from "@tanstack/react-query";
+import type {Todo} from "../types/todo.ts";
+import {createTodo} from "./api.ts";
+
+export function useCreateTodo() {
+    return useMutation({
+        mutationFn: (data: Todo) => createTodo(data),
+        onMutate: () => {
+            console.log("Mutated")
+        },
+
+        onError: () => {
+            console.log("Error")
+        },
+        onSuccess: () => {
+            console.log("Success")
+        },
+        onSettled: () => {
+            console.log("Settled")
+        }
+    })
+}
